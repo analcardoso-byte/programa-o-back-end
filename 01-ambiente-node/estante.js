7ff7028024cf046cfbdadd5eddf67f5b893ecbf9
analaura <ana.l.cardoso@edu.senai.br>
2026-07-31 15:16:12 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/estante.js
@@ -0,0 +1,18 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+titulo: "Clean Code",
+autor: "Robert C. Martin",
+preco: 89.9,
+estoque: 12,
+};
+
+// ===== LIVRO 2  =====
+const livro2 = {
+titulo: "Caraval",
+autor: "Stephanie Garber",
+preco: 50.0,
+estoque: 7,
+};
+
+// Esta linha entrega os dois livros para os outros arquivos usarem
+module.exports = { livro1, livro2 };
